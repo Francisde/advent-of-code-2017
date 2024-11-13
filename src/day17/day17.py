46c20cc7ca9c42b3c9b3a300496a910f9b258121
Martin Mehlhose <mehlhose@rvs.informatik.uni-leipzig.de>
2024-11-13 16:05:31 +0100

    optimize day 17

--- src/day17/day17.py
+++ src/day17/day17.py
@@ -2,8 +2,6 @@ def solve_part_one(steps, step_size):
     current_position = 0
     buffer = [0]
     for i in range(1, steps):
-        if i % 1000000 == 0:
-            print(i)
         new_index = get_new_position(buffer, current_position, step_size)
         current_position = new_index
         buffer.insert(current_position + 1, i)
@@ -16,21 +14,36 @@ def solve_part_one(steps, step_size):
 def solve_part_two(steps, step_size):
     current_position = 0
     buffer = [0]
+    last_insert_on_one = None
+    buffer_length = 1
     for i in range(1, steps):
-        if i % 1000000 == 0:
-            print(i/1000000)
-        new_index = get_new_position(buffer, current_position, step_size)
+        new_index = get_new_position_two(buffer_length, current_position, step_size)
         current_position = new_index
-        buffer.insert(current_position + 1, i)
+        if current_position + 1 == 1:
+            last_insert_on_one = i
         current_position += 1
+        buffer_length += 1
 
-    return buffer[:5]
+    return last_insert_on_one
 
 
 def get_new_position(input_list, current_position, steps):
     new_index = current_position
+    if current_position + steps < len(input_list) - 5:
+        return current_position + steps
     for i in range(steps):
         if new_index == len(input_list) - 1:
+            new_index = 0
+        else:
+            new_index += 1
+    return new_index
+
+def get_new_position_two(input_list_length, current_position, steps):
+    new_index = current_position
+    if current_position + steps < input_list_length - 5:
+        return current_position + steps
+    for i in range(steps):
+        if new_index == input_list_length - 1:
             new_index = 0
         else:
             new_index += 1
